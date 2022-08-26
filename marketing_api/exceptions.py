80d6a714315edf1e53d0928045672f4132220e08
semen603089 <semen603089@mail.ru>
2022-08-26 23:48:46 +0300

    review

--- marketing_api/exceptions.py
+++
@@ -1,8 +0,0 @@
-class ActionError(Exception):
-    def __init__(self, action: str):
-        super().__init__(f"Invalid action: {action}")
-
-
-class DB_Error(Exception):
-    def __init__(self):
-        super().__init__(f"Error with db")
--- marketing_api/routes/models.py
+++ marketing_api/routes/models.py
@@ -1,7 +1,4 @@
-import datetime
-from pydantic import BaseModel, AnyHttpUrl, validator
-from marketing_api.models.db import Actions
-from marketing_api.exceptions import ActionError
+from pydantic import BaseModel, AnyHttpUrl
 
 
 class Base(BaseModel):
