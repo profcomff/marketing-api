0ff6e2580b90a437ca56c50d516245918d287a40
semen603089 <semen603089@mail.ru>
2022-08-26 23:40:18 +0300

    review

--- marketing_api/routes/action.py
+++ marketing_api/routes/action.py
@@ -8,6 +8,6 @@ from marketing_api.methods.db_utils import add_action_from_user_info
 action_router = APIRouter(prefix='/action', tags=['user_actions'])
 
 
-@action_router.post('/v1/post')
+@action_router.post('/v1/action')
 def write_action(user_action_info: ActionInfo):
     add_action_from_user_info(user_action_info, db.session)
