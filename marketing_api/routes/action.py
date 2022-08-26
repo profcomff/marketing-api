b8ecb73a6499a7dc90329087a0874464b06423a6
semen603089 <semen603089@mail.ru>
2022-08-26 23:42:21 +0300

    review

--- marketing_api/routes/action.py
+++ marketing_api/routes/action.py
@@ -1,13 +1,18 @@
-import sqlalchemy
 from fastapi import APIRouter
-from .models import ActionInfo
 from fastapi_sqlalchemy import db
 from sqlalchemy.exc import SQLAlchemyError
-from marketing_api.methods.db_utils import add_action_from_user_info
+
+from .models import ActionInfo
+from ..exceptions import DB_Error
+from ..models.db import ActionsInfo
 
 action_router = APIRouter(prefix='/action', tags=['user_actions'])
 
 
 @action_router.post('/v1/action')
 def write_action(user_action_info: ActionInfo):
-    add_action_from_user_info(user_action_info, db.session)
+    try:
+        db.session.add(ActionsInfo(**user_action_info.dict()))
+        db.session.flush()
+    except SQLAlchemyError:
+        raise DB_Error
