cf4d12adb33ea150890fb4222adacaf546984c69
Mikhail <mmikee00800@gmail.com>
2022-09-08 19:24:55 +0300

    test upd + patch

--- marketing_api/routes/base.py
+++ marketing_api/routes/base.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi.exceptions import HTTPException
 from fastapi_sqlalchemy import db
 from starlette.responses import PlainTextResponse
-
+from sqlalchemy.exc import IntegrityError
+import starlette
 from marketing_api import get_settings
 from marketing_api.models import ActionsInfo
 from .models import ActionInfo, User
@@ -15,9 +17,12 @@ app = FastAPI()
 
 @app.post('/v1/action')
 async def write_action(user_action_info: ActionInfo):
-    db.session.add(ActionsInfo(**user_action_info.dict()))
-    db.session.flush()
-    return PlainTextResponse(status_code=204)
+    try:
+        db.session.add(ActionsInfo(**user_action_info.dict()))
+        db.session.flush()
+        return PlainTextResponse(status_code=204)
+    except IntegrityError:
+        return PlainTextResponse("Invalid data", status_code=500)
 
 
 @app.post('/v1/user', response_model=User)
@@ -28,8 +33,19 @@ async def create_user():
     return user
 
 
+@app.patch('/v1/user/{id}', response_model=User)
+async def patch_user(id: int, union_number: int):
+    result: DbUser = db.session.query(DbUser).filter(DbUser.id == id).one_or_none()
+    if not result:
+        raise HTTPException(404, "No user found")
+    else:
+        result.union_number = union_number
+        db.session.flush()
+        return result
+
+
 @app.exception_handler(Exception)
-def http_error_handler():
+def http_error_handler(req, exc):
     return PlainTextResponse("Error", status_code=500)
 
 
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,10 +1,21 @@
 from fastapi.testclient import TestClient
 
 from marketing_api.routes.base import app
-from marketing_api.routes.models import ActionInfo
+from marketing_api.routes.models import ActionInfo, User
 
 
-def test_post():
+def test_can_post_without_user_id():
+    client = TestClient(app)
+    action = ActionInfo(
+        action="INSTALL",
+        path_from="http://127.0.0.1:8000/",
+        path_to="http://127.0.0.1:8000/me"
+    )
+    response = client.post(f"/v1/action", action.json())
+    assert response.status_code == 204
+
+
+def test_can_post_with_user_id():
     client = TestClient(app)
     action = ActionInfo(
         user_id=1,
@@ -16,8 +27,28 @@ def test_post():
     assert response.status_code == 204
 
 
+def test_cannot_post_invalid_info():
+    client = TestClient(app)
+    action = ActionInfo(
+        action="INSTALL",
+        path_to="http://127.0.0.1:8000/me",
+    )
+    response = client.post(f"/v1/action", action.json())
+    assert response.status_code == 500
+
+
+def test_can_patch_user():
+    client = TestClient(app)
+    db_user = client.post("/v1/user")
+    user = User(**db_user.json())
+    client.patch(f"/v1/user/{user.id}?union_number={406}")
+
+
 def test_user_create():
     client = TestClient(app)
     response = client.post("/v1/user")
     assert response.status_code == 200
 
+
+if __name__ == "__main__":
+    test_can_patch_user()
