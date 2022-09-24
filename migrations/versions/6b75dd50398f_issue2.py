70441d6fc588a14a385efa592bb23c4e2b414c26
Mikhail <mmikee00800@gmail.com>
2022-09-24 20:59:54 +0300

    migrations

--- migrations/versions/6b75dd50398f_issue2.py
+++ migrations/versions/6b75dd50398f_issue2.py
@@ -1,3 +1,5 @@
+import datetime
+
 from alembic import op
 import sqlalchemy as sa
 
@@ -11,8 +13,12 @@ depends_on = None
 def upgrade():
     op.add_column('actions_info', sa.Column('additional_data', sa.String(), nullable=True))
     op.add_column('user', sa.Column('union_number', sa.String(), nullable=True))
-    op.add_column('user', sa.Column('modify_ts', sa.DateTime(), nullable=False))
-    op.add_column('user', sa.Column('create_ts', sa.DateTime(), nullable=False))
+    op.add_column('user', sa.Column('modify_ts', sa.DateTime(), nullable=True))
+    op.execute(f"UPDATE user SET modify_ts = {datetime.datetime.utcnow()}")
+    op.alter_column('user', 'modify_ts', nullable=False)
+    op.add_column('user', sa.Column('create_ts', sa.DateTime(), nullable=True))
+    op.execute(f"UPDATE user SET create_ts = {datetime.datetime.utcnow()}")
+    op.alter_column('user', 'create_ts', nullable=False)
 
 
 def downgrade():
